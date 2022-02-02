8ca12c1a38341b2fe4cbb263a5ca01005ffb1ed8
gua21150 <gua21150@uvg.edu.gt>
2022-02-01 21:27:30 -0600

    Creación de interfaces
    Se agregaron las interfaces discutidas con los demás grupos y el inicio de la clase pila.java

---
+++ javaproject/src/main/java/com/Calculator.java
@@ -0,0 +1,10 @@
+package com;
+
+public interface Calculator {
+    /**
+     * Calculates an entire operation.
+     * @param operation String containing the operation to be calculated.
+     * @return double with the result of the operation
+     */
+    public double calculate(String operation);
+}
---
+++ javaproject/src/main/java/com/Pila.java
@@ -0,0 +1,41 @@
+package com;
+
+public class Pila implements Stack<Double>, Calculator{
+
+    @Override
+    public double calculate(String operation) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void add(Double item) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Double remove() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Double peek() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean empty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+}
